01d2ea815301f5e4a4d7f49b367cc3659e2d8541
Priya Aswani <priya.aswani28139@gmail.com>
2022-02-01 16:05:52 +0400

    Export images via Images

---
+++ src/constants/Images.js
@@ -0,0 +1,13 @@
+export const Images = {
+  bedroom: require('../assets/icons/bedroom.png'),
+  door: require('../assets/icons/door.png'),
+  gardening: require('../assets/icons/gardening.png'),
+  large_leaves: require('../assets/icons/large-leaves.png'),
+  living_room: require('../assets/icons/living-room.png'),
+  logo: require('../assets/icons/logo.png'),
+  plant_in_vase: require('../assets/icons/plant-in-vase.png'),
+  potted_plant: require('../assets/icons/potted_plant.png'),
+  small_leaves: require('../assets/icons/small-leaves.png'),
+  user: require('../assets/icons/user.png'),
+  wall_unit: require('../assets/icons/wall-unit.png'),
+};
