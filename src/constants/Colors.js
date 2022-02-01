8586f5cab1ebb5e2d3c5829694118dedb779ccb9
Priya Aswani <priya.aswani28139@gmail.com>
2022-02-01 16:04:55 +0400

    Export all colors via Colors

---
+++ src/constants/Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  blue: '#0AC4BA',
+  darkGray: '#444444',
+  darkerGray: '#323643',
+  green: '#2BDA8E',
+  lightGray: '#C5CCD6',
+  mediumGray: '#9DA3B4',
+  red: '#F3534A',
+  white: '#FFFFFF',
+};
