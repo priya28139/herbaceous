4f15dc6b3d0077d93183ac2a18a9e89100c9c7dc
Priya Aswani <priya.aswani28139@gmail.com>
2022-02-01 16:05:07 +0400

    Export all icons via Icons

---
+++ src/constants/Icons.js
@@ -0,0 +1,11 @@
+export const Icons = {
+  back: require('../assets/icons/back.png'),
+  fertilizer: require('../assets/icons/fertilizer.png'),
+  flower: require('../assets/icons/flower.png'),
+  plant: require('../assets/icons/plant.png'),
+  pot: require('../assets/icons/pot.png'),
+  search: require('../assets/icons/search.png'),
+  seed: require('../assets/icons/seed.png'),
+  sprayer: require('../assets/icons/sprayer.png'),
+  view: require('../assets/icons/view.png'),
+};
