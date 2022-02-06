1dbffcf44e07530e38365d54dc8e1154c7231a6f
Priya Aswani <priya.aswani28139@gmail.com>
2022-02-06 15:59:59 +0400

    Export HorizontalBar from components' index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Button } from './inputs/Button';
 export { FormTextInput } from './inputs/FormTextInput';
+export { HorizontalBar } from './accessories/HorizontalBar';
 export { NavigationHeader } from './accessories/NavigationHeader';
